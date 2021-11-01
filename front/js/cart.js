2c57fda93eef478be73b82806e640ed390f3c27b
Ludovic <ludovic.porlan@gmail.com>
2021-11-01 15:44:48 +0100

    Displaying Carts products

--- front/js/cart.js
+++ front/js/cart.js
@@ -10,5 +10,39 @@ class cart {
     static save(cart) {
         localStorage.setItem("cart", JSON.stringify(cart));
     }
-    // Afficher le panier
+}
+
+(async function () {
+    // récupération des produits du panier
+    const productsInCart = await cart.getCart();
+    // Affichage de tout les produits contenu dans le panier
+    for (product of productsInCart) {
+        displayCartsProduct(product);
+    }
+})();
+
+function displayCartsProduct() {
+    document.getElementById(
+        "cart__items"
+    ).innerHTML += `<article class="cart__item" data-id="${product._id}" data-color="${product.color}">
+    <div class="cart__item__img">
+        <img src="${product.imageUrl}" alt="Photographie d'un canapé ${product.name}">
+    </div>
+    <div class="cart__item__content">
+        <div class="cart__item__content__titlePrice">
+            <h2>${product.name}</h2>
+            <p>${product.price} €</p>
+            <p>${product.color}</p>
+        </div>
+        <div class="cart__item__content__settings">
+            <div class="cart__item__content__settings__quantity">
+                <p>Qté : </p>
+                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+            </div>
+            <div class="cart__item__content__settings__delete">
+                <p class="deleteItem" id="[item_Id]">Supprimer</p>
+            </div>
+        </div>
+    </div>
+</article>`;
 }
