e1e451b37273d7662f6befe29934225a8e830c11
Ludovic <ludovic.porlan@gmail.com>
2021-11-01 09:33:37 +0100

    Add index.js déclare functions

---
+++ front/js/index.js
@@ -0,0 +1,17 @@
+// On fait du fonctionnel donc on met toutes les variables dans des fonctions.
+main();
+
+function main() {
+    // récupération des produits
+    const products = getProducts();
+    // Affichage de tout les produits
+    displayAllProducts(products);
+}
+
+function getProducts() {
+    return "";
+}
+
+function displayAllProducts() {
+    return "";
+}
