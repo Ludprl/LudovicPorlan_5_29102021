7ee90f627bfee9eb2e2f995cda92731e73b4a76e
Ludovic <ludovic.porlan@gmail.com>
2021-11-09 12:28:41 +0100

    API url bugfix

--- front/js/config.js
+++ front/js/config.js
@@ -1,4 +1,4 @@
 let apiUrl =
     location.hostname === "localhost" || location.hostname === "127.0.0.1"
         ? "http://localhost:3000"
-        : "https://ocr-p5-kanap.herokuapp.com/";
+        : "https://ocr-p5-kanap.herokuapp.com";
