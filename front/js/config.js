bec04fde8894fbf007e1044206dc7bf9bded28a8
Ludovic <ludovic.porlan@gmail.com>
2021-12-02 21:30:56 +0100

    add some comments

--- front/js/config.js
+++ front/js/config.js
@@ -1,3 +1,4 @@
+// Check de l'adresse pour savoir si il faut chercher l'API en local ou sur son adresse d'hébergement.
 let apiUrl =
     location.hostname === "localhost" || location.hostname === "127.0.0.1"
         ? "http://localhost:3000"
--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,3 +1,4 @@
+// Affichage de la confirmation de commande en retournant l'orderID
 (function () {
     let orderid = new URL(location.href).searchParams.get("orderid");
     document.getElementById("orderId").insertAdjacentHTML("beforeend", orderid);
