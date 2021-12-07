9121cc808df37c5d46a3b692ae466034c0a016ef
Ludovic <ludovic.porlan@gmail.com>
2021-12-07 13:41:53 +0100

    Fix an error on delete item in cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -94,7 +94,6 @@ function onDeleteCartItem() {
                 );
                 toRemove.remove();
                 cart.remove(productId, productColor);
-                displayCartsProduct();
             }
         });
     });
